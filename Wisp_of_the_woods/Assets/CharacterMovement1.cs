a4b8f84b75d599ca68d824f8623eb86495963592
juanibar10 <47811666+juanibar10@users.noreply.github.com>
2019-12-02 18:54:45 +0100

    .

--- Wisp_of_the_woods/Assets/CharacterMovement1.cs
+++ Wisp_of_the_woods/Assets/CharacterMovement1.cs
@@ -9,6 +9,8 @@ public class CharacterMovement1 : MonoBehaviour
     
     void FixedUpdate()
     {
+        GetComponent<Animator>().SetFloat("Vertical1", Input.GetAxis("Horizontal"));
+        GetComponent<Animator>().SetFloat("Vertical", Input.GetAxis("Vertical"));
         if(Input.GetAxis("Horizontal") > 0)
             transform.Translate(Vector3.forward * Input.GetAxis("Horizontal") * velocidad * Time.fixedDeltaTime, Space.Self);
         else if(Input.GetAxis("Horizontal") < 0)
