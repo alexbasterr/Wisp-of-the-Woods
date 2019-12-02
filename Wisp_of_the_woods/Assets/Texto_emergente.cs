53f0806f277f7e1841ceecddf57d6964311caa1c
alexbasterr <48485378+alexbasterr@users.noreply.github.com>
2019-12-02 03:29:36 +0100

    1

---
+++ Wisp_of_the_woods/Assets/Texto_emergente.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Texto_emergente : MonoBehaviour
+{
+    public GameObject go;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        go.SetActive(true);
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+        go.SetActive(false);
+    }
+}
