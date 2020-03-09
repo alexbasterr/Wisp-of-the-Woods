2ddf583efb2c5b0a873618efd051db9692de60db
Julen Bordonaba <48485297+JulenBordonaba@users.noreply.github.com>
2020-03-09 23:49:52 +0100

    ReplaceObjects Tool

---
+++ Wisp_of_the_woods/Assets/Editor/ReplaceObjectEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ReplaceObject))]
+public class ReplaceObjectEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        ReplaceObject snapToGround = (ReplaceObject)target;
+
+        if (GUILayout.Button("Replace Objects"))
+        {
+            snapToGround.Replace();
+        }
+
+    }
+}
