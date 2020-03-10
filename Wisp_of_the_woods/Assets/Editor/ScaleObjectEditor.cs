fa488928caa2aa9301f04340817295b12f24117f
alexbasterr <48485378+alexbasterr@users.noreply.github.com>
2020-03-10 14:07:25 +0100

    jejeje

---
+++ Wisp_of_the_woods/Assets/Editor/ScaleObjectEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ScaleObject))]
+public class ScaleObjectEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        ScaleObject snapToGround = (ScaleObject)target;
+
+        if (GUILayout.Button("Scale Objects"))
+        {
+            snapToGround.Scale();
+        }
+
+    }
+}
