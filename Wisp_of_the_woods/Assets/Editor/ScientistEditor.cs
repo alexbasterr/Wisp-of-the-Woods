c2257d1cb3ec0829186310712470b31e8d3292b7
alexbasterr <48485378+alexbasterr@users.noreply.github.com>
2020-03-10 12:22:30 +0100

    Magia

---
+++ Wisp_of_the_woods/Assets/Editor/ScientistEditor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ScientistCheckpoints))]
+public class ScientistEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        ScientistCheckpoints tar = (ScientistCheckpoints)target;
+        
+        if(GUILayout.Button("Show Checkpoints"))
+        {
+            foreach (Transform t in tar.scientist.Checkpoints)
+            {
+                t.gameObject.GetComponent<MeshRenderer>().enabled = true;
+            }
+        }
+
+        if (GUILayout.Button("Hide Checkpoints"))
+        {
+            foreach (Transform t in tar.scientist.Checkpoints)
+            {
+                t.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
+
+
+
+    }
+}
